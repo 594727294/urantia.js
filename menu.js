d02774f45f095908ab566900c219552cd4c622e9
~~~ <xcxdxix@gmail.com>
2015-09-27 02:30:17 -0300

    0.01

---
+++ menu.js
@@ -0,0 +1,7 @@
+function fontSwitch(type){
+	if(type="serif"){
+		$('.document').css('font-family', 'TimesNewRoman, "Times New Roman", Times, Baskerville, Georgia, serif');
+	}else{
+		$('.document').css('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif');
+	}
+}
